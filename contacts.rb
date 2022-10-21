cc2d4c3e499f71f7b0f1ebe4f16374abae6e1614
Stephen <skairu09@gmail.com>
2022-10-21 03:22:09 +0300

    Completed assignment

--- contacts.rb
+++ contacts.rb
@@ -17,8 +17,40 @@ def contacts
   }
 end
 
+# def remove_strawberry(contacts)
+#   # your code here!
+#   contacts.each do |person, contact_details_hash|
+#     binding.pry
+#     if person == "Freddie Mercury"
+#       contact_details_hash.each do |attributes, data|
+#     binding.pry
+#       end
+#     end
+#   end
+# end
+
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     if person == "Freddy Mercury"
+#       contact_details_hash.each do |attribute, data|
+#         if attribute == :favorite_ice_cream_flavors
+#           data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#         end
+#       end
+#     end
+#   end
+# end
+
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
